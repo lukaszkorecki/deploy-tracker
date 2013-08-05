2bad89621601758bff854a0d5184f57e70d60935
Łukasz Korecki <lukasz@coffeesounds.com>
2013-08-05 21:13:45 +0100

    some scratchpad-style code

--- app.rb
+++ app.rb
@@ -5,8 +5,7 @@ class DeployTracker < Sinatra::Base
     # main html and such
   end
 
-
-  get "/events" do
+  get "/:project_id/events" do
     # list events
   end
 
@@ -14,7 +13,24 @@ class DeployTracker < Sinatra::Base
     # single event
   end
 
-  post "/events" do
-    # create a new one
+  post "/:project_id/events" do |project_id|
+    payload = JSON.parse request['rack.input'].read
+    actor = Actor.find(api_key: params["api_key"]).first
+    project = Project.find(id: project_id, team_id: actor.team_id).first
+
+    if actor and project
+      new_event = Event.create(
+        actor_id: actor.id,
+        project_id: project.id,
+        name: payload["name"],
+        revision: payload["revision"],
+        metadata: payload["metadata"]
+      )
+
+      EventPresenter.new(new_event).to_json
+    else
+      Error.new("nope!").respond_with 404
+    end
   end
+
 end
